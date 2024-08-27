0c245e51aae6da28523761c78910f511dec3b42a
@skye <sonicgames772@gmail.com>
2024-08-27 17:12:36 -0400

    Remove trash

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Mon Aug 26 14:11:23 EDT 2024"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "mypa1n"
-#define LINUX_COMPILER "Neutron clang version 18.0.0git (https://github.com/llvm/llvm-project.git ca5d34ec7186f2b5750c7e67dcb8b2d0dc865d8d), Neutron LLD 18.0.0"
